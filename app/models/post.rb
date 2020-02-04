7498264f72b3f98302d5be59d1b918aa1877c30d
Dan Clark <wpu.clark@gmail.com>
2020-02-04 08:25:43 -0500

    Add fenced code blocks and an edit button to the show post page.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   def self.markdown
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
   end
 end
